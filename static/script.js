c8f8f078d2025bb2388934c89390f2fcb0faa185
sieczkah <huberts94@gmail.com>
2023-06-08 15:21:43 +0200

    Added about me section

---
+++ static/script.js
@@ -0,0 +1,9 @@
+// Show timeline
+$(document).ready(function () {
+    $(".show-timeline").click(function () {
+        $("#timeline").toggle();
+        $(this).text(function (i, text) {
+            return text === "Show timeline" ? "Hide timeline" : "Show timeline";
+        });
+    });
+});
