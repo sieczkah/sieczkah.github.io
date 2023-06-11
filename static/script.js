b07b57462883b2cc6cbaeedb1603e72d90d496fb
sieczkah <huberts94@gmail.com>
2023-06-11 22:42:39 +0200

    Added page general content and styling

--- static/script.js
+++ static/script.js
@@ -6,6 +6,30 @@ $(document).ready(function () {
             return text === "Show timeline" ? "Hide timeline" : "Show timeline";
         });
     });
+    const counterUp = window.counterUp.default;
+
+    const callback = entries => {
+        entries.forEach(entry => {
+            const el = entry.target;
+            if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+                counterUp(el, {
+                    duration: 1300,
+                    delay: 16,
+                });
+                el.classList.add('is-visible');
+            }
+        });
+    };
+
+    const IO = new IntersectionObserver(callback, { threshold: 1 });
+
+    const counters = document.querySelectorAll('.counter'); // Select all counter elements
+
+    counters.forEach(counter => {
+        IO.observe(counter); // Observe each counter element individually
+    });
+
+
+
 
 });
-
